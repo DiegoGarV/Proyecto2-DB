c84ceabb2f636e1f5b819e4470f2f6f7443c04d7
Cesar Lopez <110710742+Czar272@users.noreply.github.com>
2025-04-13 21:01:55 -0600

    chore: clean code and hide conection key

--- bulkwriteToAtlas.py
+++ bulkwriteToAtlas.py
@@ -1,11 +1,14 @@
 import pandas as pd
 from pymongo import MongoClient, InsertOne
+from dotenv import load_dotenv
 import os
 
 # Configurar URI de conexión
-MONGO_URI = "mongodb+srv://diegoG:Patron12@basedatos2.t66ja.mongodb.net/"
+load_dotenv()
+MONGO_URI = os.getenv("MONGO_URI")
 client = MongoClient(MONGO_URI)
 db = client["proyecto2-db"]
+
 
 # Función para cargar e insertar un CSV
 def insertar_csv_en_mongo(nombre_csv, nombre_coleccion):
@@ -13,9 +16,12 @@ def insertar_csv_en_mongo(nombre_csv, nombre_coleccion):
     operaciones = [InsertOne(row.dropna().to_dict()) for _, row in df.iterrows()]
     if operaciones:
         resultado = db[nombre_coleccion].bulk_write(operaciones)
-        print(f"✅ Insertados {resultado.inserted_count} documentos en '{nombre_coleccion}'")
+        print(
+            f"✅ Insertados {resultado.inserted_count} documentos en '{nombre_coleccion}'"
+        )
     else:
         print(f"⚠️ No hay documentos para insertar en '{nombre_coleccion}'")
+
 
 # Inserta cada CSV
 insertar_csv_en_mongo("usuarios.csv", "usuarios")
